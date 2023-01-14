a75c55e0d7873589ef4e2003fefc3bbf5af1888a
Черепахин Василий <vasi.che@gmail.com>
2023-01-14 14:11:20 +0500

    Test Error 503

--- src/test/java/ru/perm/v/parking/controller/CarControllerTest.java
+++ src/test/java/ru/perm/v/parking/controller/CarControllerTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import ru.perm.v.parking.controller.exception.BadGatewayException;
 import ru.perm.v.parking.controller.exception.Error502;
 import ru.perm.v.parking.controller.exception.Error503;
 import ru.perm.v.parking.db.CarEntity;
@@ -13,11 +12,9 @@ import ru.perm.v.parking.service.CarService;
 
 import javax.persistence.EntityNotFoundException;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class CarControllerTest {
-    CarService carService = mock(CarService.class);
 
     @Test
     void newCar() {
@@ -28,6 +25,7 @@ class CarControllerTest {
         carEntity.setGosNumber(gosNumber);
         carEntity.setModel(model);
 
+        CarService carService = mock(CarService.class);
         CarController carController = new CarController(carService);
 
         when(carService.registration(gosNumber, model)).thenReturn(carEntity);
@@ -46,26 +44,16 @@ class CarControllerTest {
         Long ID = 100L;
         CarEntity deletedCar = new CarEntity();
         deletedCar.setId(ID);
+        CarService carService = mock(CarService.class);
         when(carService.getById(ID)).thenReturn(deletedCar);
         when(carService.deleteById(ID)).thenReturn(true);
         CarController carController = new CarController(carService);
 
         ResponseEntity<String> ret = carController.deleteCar(ID);
 
+        verify(carService, times(1)).getById(ID);
+        verify(carService, times(1)).deleteById(ID);
         Assertions.assertEquals(HttpStatus.OK, ret.getStatusCode());
-    }
-
-    @Test
-    void getCarWithNormalID() {
-        Long ID = 100L;
-        CarEntity car = new CarEntity();
-        car.setId(ID);
-        when(carService.getById(ID)).thenReturn(car);
-        CarController carController = new CarController(carService);
-
-        CarDto carDtoReceived = carController.getCar(ID);
-
-        Assertions.assertEquals(ID, carDtoReceived.getId());
     }
 
     @Test
@@ -73,16 +61,39 @@ class CarControllerTest {
         Long ID = 100L;
         CarEntity car = new CarEntity();
         car.setId(ID);
-        CarController carController = new CarController(carService);
+        CarService carService = mock(CarService.class);
         when(carService.getById(ID)).thenThrow(EntityNotFoundException.class);
+        CarController carController = new CarController(carService);
+
+//      Не сработает, т.к. замокан when(carService.getById(ID)) через thenThrow
+//      verify(carService, times(1)).getById(any(Long.class));
 
         Assertions.assertThrows(Error502.class, () -> carController.getCar(ID));
     }
 
     @Test
+    void getCarWithNormalID() {
+        Long ID = 100L;
+        CarEntity car = new CarEntity();
+        car.setId(ID);
+        CarService carService = mock(CarService.class);
+        when(carService.getById(ID)).thenReturn(car);
+        CarController carController = new CarController(carService);
+
+        CarDto carDtoReceived = carController.getCar(ID);
+
+        verify(carService, times(1)).getById(ID);
+        Assertions.assertEquals(ID, carDtoReceived.getId());
+    }
+
+    @Test
     void getOtherErrorForGetCar() {
         Long ID = 100L;
+        CarService carService = mock(CarService.class);
+
         CarController carController = new CarController(carService);
+
         Assertions.assertThrows(Error503.class, () -> carController.getCar(ID));
+        verify(carService, times(1)).getById(ID);
     }
 }
